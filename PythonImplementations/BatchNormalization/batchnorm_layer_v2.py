90ef173d80ca48d6d74deb0a358d87dfce37e001
Juan Sebastián Barreto Jiménez <juan_barreto@javeriana.edu.co>
2021-11-15 10:48:59 -0500

    Se sube excel de BN

--- PythonImplementations/BatchNormalization/batchnorm_layer_v2.py
+++ PythonImplementations/BatchNormalization/batchnorm_layer_v2.py
@@ -11,13 +11,13 @@ epsilon = 0#.000001
 data_x = [1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6]
 data_y = [0.169610272, 0.283395813, 0.386358738, 0.470227872, 0.433281294, 0.600267648, 0.73833898, 0.79031502, 0.877464268, 0.843564462, 0.964438917]
 
-bn = tf.keras.layers.BatchNormalization(epsilon=epsilon,beta_initializer='zeros',gamma_initializer='ones',moving_mean_initializer='zeros',moving_variance_initializer='ones')
+#bn = tf.keras.layers.BatchNormalization(epsilon=epsilon,beta_initializer='zeros',gamma_initializer='ones',moving_mean_initializer='zeros',moving_variance_initializer='ones')
 
-model = tf.keras.Sequential(bn)
-model.compile(optimizer='sgd',loss='mean_squared_error')
+# model = tf.keras.Sequential(bn)
+# model.compile(optimizer='sgd',loss='mean_squared_error')
 
-pprint(dir(bn))
+print("moving_mean:", bn.moving_mean.numpy())
 #model.fit(x=data_x, y=data_y, epochs=500)
 
 
-print(model.predict(data_x))
+#print(model.predict(data_x))
